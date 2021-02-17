4a34353bb7d3dc797943ea043dae854ac4aa7c16
Arturo <Arturo@919647e8-3121-2f49-b100-017848d5475d>
2021-02-17 09:38:57 +0000

    Ajustes para el Simulador de SACTA
    git-svn-id: http://10.34.58.18/svn/sacta-proxy/trunk@37 919647e8-3121-2f49-b100-017848d5475d

--- SimulSacta/MainForm.Designer.cs
+++ SimulSacta/MainForm.Designer.cs
@@ -39,6 +39,10 @@ namespace SimulSACTA
             this.comboBox1 = new System.Windows.Forms.ComboBox();
             this.label1 = new System.Windows.Forms.Label();
             this.panel1 = new System.Windows.Forms.Panel();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lan1Info = new System.Windows.Forms.Label();
+            this.lan2Info = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
             _InfoCM = new System.Windows.Forms.ContextMenuStrip(this.components);
             _MainMS = new System.Windows.Forms.MenuStrip();
             _InfoCM.SuspendLayout();
@@ -48,32 +52,34 @@ namespace SimulSACTA
             // 
             // _InfoCM
             // 
+            _InfoCM.ImageScalingSize = new System.Drawing.Size(20, 20);
             _InfoCM.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
             this.clearToolStripMenuItem});
             _InfoCM.Name = "_InfoCM";
-            _InfoCM.Size = new System.Drawing.Size(102, 26);
+            _InfoCM.Size = new System.Drawing.Size(113, 28);
             // 
             // clearToolStripMenuItem
             // 
             this.clearToolStripMenuItem.Name = "clearToolStripMenuItem";
-            this.clearToolStripMenuItem.Size = new System.Drawing.Size(101, 22);
+            this.clearToolStripMenuItem.Size = new System.Drawing.Size(112, 24);
             this.clearToolStripMenuItem.Text = "Clear";
             this.clearToolStripMenuItem.Click += new System.EventHandler(this.clearToolStripMenuItem_Click);
             // 
             // _MainMS
             // 
+            _MainMS.ImageScalingSize = new System.Drawing.Size(20, 20);
             _MainMS.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
             this.settingsToolStripMenuItem});
             _MainMS.Location = new System.Drawing.Point(0, 0);
             _MainMS.Name = "_MainMS";
-            _MainMS.Size = new System.Drawing.Size(522, 24);
+            _MainMS.Size = new System.Drawing.Size(696, 28);
             _MainMS.TabIndex = 8;
             _MainMS.Text = "menuStrip1";
             // 
             // settingsToolStripMenuItem
             // 
             this.settingsToolStripMenuItem.Name = "settingsToolStripMenuItem";
-            this.settingsToolStripMenuItem.Size = new System.Drawing.Size(84, 20);
+            this.settingsToolStripMenuItem.Size = new System.Drawing.Size(106, 24);
             this.settingsToolStripMenuItem.Text = "Propiedades";
             this.settingsToolStripMenuItem.Click += new System.EventHandler(this.settingsToolStripMenuItem_Click);
             // 
@@ -81,16 +87,19 @@ namespace SimulSACTA
             // 
             this._InfoLB.ContextMenuStrip = _InfoCM;
             this._InfoLB.FormattingEnabled = true;
-            this._InfoLB.Location = new System.Drawing.Point(3, 114);
+            this._InfoLB.ItemHeight = 16;
+            this._InfoLB.Location = new System.Drawing.Point(4, 181);
+            this._InfoLB.Margin = new System.Windows.Forms.Padding(4);
             this._InfoLB.Name = "_InfoLB";
-            this._InfoLB.Size = new System.Drawing.Size(482, 186);
+            this._InfoLB.Size = new System.Drawing.Size(641, 420);
             this._InfoLB.TabIndex = 5;
             // 
             // _SectorizeBT
             // 
-            this._SectorizeBT.Location = new System.Drawing.Point(410, 85);
+            this._SectorizeBT.Location = new System.Drawing.Point(545, 145);
+            this._SectorizeBT.Margin = new System.Windows.Forms.Padding(4);
             this._SectorizeBT.Name = "_SectorizeBT";
-            this._SectorizeBT.Size = new System.Drawing.Size(75, 23);
+            this._SectorizeBT.Size = new System.Drawing.Size(100, 28);
             this._SectorizeBT.TabIndex = 4;
             this._SectorizeBT.Text = "Enviar";
             this._SectorizeBT.UseVisualStyleBackColor = true;
@@ -98,50 +107,95 @@ namespace SimulSACTA
             // 
             // _SectorsTB
             // 
-            this._SectorsTB.Location = new System.Drawing.Point(15, 39);
+            this._SectorsTB.Location = new System.Drawing.Point(20, 115);
+            this._SectorsTB.Margin = new System.Windows.Forms.Padding(4);
             this._SectorsTB.Name = "_SectorsTB";
-            this._SectorsTB.Size = new System.Drawing.Size(470, 20);
+            this._SectorsTB.Size = new System.Drawing.Size(625, 22);
             this._SectorsTB.TabIndex = 3;
             // 
             // comboBox1
             // 
             this.comboBox1.FormattingEnabled = true;
-            this.comboBox1.Location = new System.Drawing.Point(189, 12);
+            this.comboBox1.Location = new System.Drawing.Point(252, 82);
+            this.comboBox1.Margin = new System.Windows.Forms.Padding(4);
             this.comboBox1.Name = "comboBox1";
-            this.comboBox1.Size = new System.Drawing.Size(296, 21);
+            this.comboBox1.Size = new System.Drawing.Size(393, 24);
             this.comboBox1.TabIndex = 9;
             this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.OnPresectChange);
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(12, 12);
+            this.label1.Location = new System.Drawing.Point(16, 82);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(139, 13);
+            this.label1.Size = new System.Drawing.Size(186, 17);
             this.label1.TabIndex = 10;
             this.label1.Text = "Sectorizaciones Preparadas";
             // 
             // panel1
             // 
             this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel1.Controls.Add(this.lan2Info);
+            this.panel1.Controls.Add(this.label4);
+            this.panel1.Controls.Add(this.lan1Info);
+            this.panel1.Controls.Add(this.label2);
             this.panel1.Controls.Add(this.label1);
             this.panel1.Controls.Add(this.comboBox1);
             this.panel1.Controls.Add(this._SectorizeBT);
             this.panel1.Controls.Add(this._InfoLB);
             this.panel1.Controls.Add(this._SectorsTB);
-            this.panel1.Location = new System.Drawing.Point(17, 27);
+            this.panel1.Location = new System.Drawing.Point(23, 33);
+            this.panel1.Margin = new System.Windows.Forms.Padding(4);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(493, 311);
+            this.panel1.Size = new System.Drawing.Size(657, 607);
             this.panel1.TabIndex = 11;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(20, 23);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(40, 17);
+            this.label2.TabIndex = 11;
+            this.label2.Text = "Lan1";
+            // 
+            // lan1Info
+            // 
+            this.lan1Info.AutoSize = true;
+            this.lan1Info.Location = new System.Drawing.Point(66, 23);
+            this.lan1Info.Name = "lan1Info";
+            this.lan1Info.Size = new System.Drawing.Size(46, 17);
+            this.lan1Info.TabIndex = 12;
+            this.lan1Info.Text = "label3";
+            // 
+            // lan2Info
+            // 
+            this.lan2Info.AutoSize = true;
+            this.lan2Info.Location = new System.Drawing.Point(66, 51);
+            this.lan2Info.Name = "lan2Info";
+            this.lan2Info.Size = new System.Drawing.Size(46, 17);
+            this.lan2Info.TabIndex = 14;
+            this.lan2Info.Text = "label3";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(20, 51);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(40, 17);
+            this.label4.TabIndex = 13;
+            this.label4.Text = "Lan2";
             // 
             // MainForm
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(522, 352);
+            this.ClientSize = new System.Drawing.Size(696, 653);
             this.Controls.Add(_MainMS);
             this.Controls.Add(this.panel1);
             this.MainMenuStrip = _MainMS;
+            this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "MainForm";
             this.Text = "SimulSACTA";
             _InfoCM.ResumeLayout(false);
@@ -164,6 +218,10 @@ namespace SimulSACTA
       private System.Windows.Forms.ComboBox comboBox1;
       private System.Windows.Forms.Label label1;
       private System.Windows.Forms.Panel panel1;
-   }
+        private System.Windows.Forms.Label lan2Info;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label lan1Info;
+        private System.Windows.Forms.Label label2;
+    }
 }
 
--- SimulSacta/SettingsForm.cs
+++ SimulSacta/SettingsForm.cs
@@ -48,28 +48,28 @@ namespace SimulSACTA
       {
          Settings sct = Settings.Default;
 
-         sct.ScvPortB = Int32.Parse(_NetBSendPortTB.Text);
-         sct.ListenPortB = Int32.Parse(_NetBListenPortTB.Text);
-         sct.ScvPortA = Int32.Parse(_NetASendPortTB.Text);
-         sct.ListenPortA = Int32.Parse(_NetAListenPortTB.Text);
-         sct.SactaGroupUser = UInt16.Parse(_SactaUserGroupTB.Text);
-         sct.SactaCenter = Byte.Parse(_SactaCenterTB.Text);
-         sct.SactaDomain = Byte.Parse(_SactaDomainTB.Text);
-         sct.SactaSPVUser = UInt16.Parse(_SPVUserTB.Text);
-         sct.SactaSPSIUser = UInt16.Parse(_SPSIUserTB.Text);
-         sct.ScvCenter = Byte.Parse(_SCVCenterTB.Text);
-         sct.ScvDomain = Byte.Parse(_SCVDomainTB.Text);
-         sct.ProcessorNumber = UInt16.Parse(_ProcessorNumberTB.Text);
-         sct.ActivityTimeOut = UInt16.Parse(_ActivityTimeoutTB.Text);
-         sct.PresenceInterval = UInt16.Parse(_PresenceIntervalTB.Text);
+         //sct.ScvPortB = Int32.Parse(_NetBSendPortTB.Text);
+         //sct.ListenPortB = Int32.Parse(_NetBListenPortTB.Text);
+         //sct.ScvPortA = Int32.Parse(_NetASendPortTB.Text);
+         //sct.ListenPortA = Int32.Parse(_NetAListenPortTB.Text);
+         //sct.SactaGroupUser = UInt16.Parse(_SactaUserGroupTB.Text);
+         //sct.SactaCenter = Byte.Parse(_SactaCenterTB.Text);
+         //sct.SactaDomain = Byte.Parse(_SactaDomainTB.Text);
+         //sct.SactaSPVUser = UInt16.Parse(_SPVUserTB.Text);
+         //sct.SactaSPSIUser = UInt16.Parse(_SPSIUserTB.Text);
+         //sct.ScvCenter = Byte.Parse(_SCVCenterTB.Text);
+         //sct.ScvDomain = Byte.Parse(_SCVDomainTB.Text);
+         //sct.ProcessorNumber = UInt16.Parse(_ProcessorNumberTB.Text);
+         //sct.ActivityTimeOut = UInt16.Parse(_ActivityTimeoutTB.Text);
+         //sct.PresenceInterval = UInt16.Parse(_PresenceIntervalTB.Text);
 
-         sct.ScvUsers.Clear();
-         for (int i = 0, total = _SCVUsersLB.Items.Count; i < total; i++)
-         {
-            sct.ScvUsers.Add(_SCVUsersLB.Items[i].ToString());
-         }
+         //sct.ScvUsers.Clear();
+         //for (int i = 0, total = _SCVUsersLB.Items.Count; i < total; i++)
+         //{
+         //   sct.ScvUsers.Add(_SCVUsersLB.Items[i].ToString());
+         //}
 
-         sct.Save();
+         //sct.Save();
       }
 
       private void AddSCVUser_Click(object sender, EventArgs e)
