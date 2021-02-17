c9987f291acfe95efb272bd2d648860dd8f75955
Arturo <Arturo@919647e8-3121-2f49-b100-017848d5475d>
2021-02-17 09:37:18 +0000

    Empezando la página de configuracion
    git-svn-id: http://10.34.2.66/svn/sacta-proxy/trunk@14 919647e8-3121-2f49-b100-017848d5475d

--- sacta-proxy/webclient/app/controllers/SprxConfig-ctrl.js
+++ sacta-proxy/webclient/app/controllers/SprxConfig-ctrl.js
@@ -4,11 +4,20 @@ angular.module("sacta_proxy")
         /** Inicializacion */
         var ctrl = this;
         ctrl.pagina = 0;
-        ctrl.config = {};
+        ctrl.config = { General: { WebPort: 1234, WebActivityMinTimeout: 45, ActivateSactaLogic: "OR" } };
+        ctrl.update = () => {
+            alertify.success("Updating config..")
+
+        };
 
         function load_config() {
             $serv.config((config) => {
-
+                if (config.res == "ok") {
+                    ctrl.config = config.cfg;
+                }
+                else {
+                    // todo
+                }
             });
         }
 
