84594e4627f3515dd1ba4b975f4ab26380ed211d
Arturo <Arturo@919647e8-3121-2f49-b100-017848d5475d>
2021-02-17 09:39:47 +0000

    Pagina de visualizacion del Historico.
    git-svn-id: http://10.34.58.18/svn/sacta-proxy/trunk@44 919647e8-3121-2f49-b100-017848d5475d

--- sacta-proxy/webclient/app/controllers/SprxStatus-ctrl.js
+++ sacta-proxy/webclient/app/controllers/SprxStatus-ctrl.js
@@ -4,7 +4,54 @@ angular.module("sacta_proxy")
         /** Inicializacion */
         var ctrl = this;
 
+        ctrl.history = null;
         ctrl.pagina = "0";
+        ctrl.SelectPage = (page) => {
+            if (ctrl.history) {
+                ctrl.history.destroy();
+                ctrl.history = null;
+            }
+            if (page == 1) {
+                ctrl.history = $('#history').DataTable({
+                    "ajax": HistoryData,
+                    "autoWidth": false,
+                    "columns": [
+                        { "data": "Date", "width": "15%", "render": (data) => ctrl.datestr(data).str },
+                        { "data": "Code", "width": "15%", "render": (data) => Enumerable.from(HistoryCodes).where(e => e.code == data).first().descr },
+                        { "data": "User", "width": "10%"},
+                        { "data": "Dep", "width": "10%" },
+                        { "data": "State", "width": "10%" },
+                        { "data": "Map", "width": "20%", "render": (map) => ctrl.normalizeMap(map).str },
+                        { "data": "Cause", "width": "20%" }
+                    ],
+                    "language": {
+                        "decimal": "",
+                        "emptyTable": "No hay datos disponibles",
+                        "info": "Registros _START_ - _END_ de _TOTAL_",
+                        "infoEmpty": "0 Registros",
+                        "infoFiltered": "(_MAX_ Registros filtrados)",
+                        "infoPostFix": "",
+                        "thousands": ".",
+                        "lengthMenu": "Muestra _MENU_ registros",
+                        "loadingRecords": "Loading...",
+                        "processing": "Processing...",
+                        "search": "Buscar:",
+                        "zeroRecords": "No se han encontrado registros",
+                        "paginate": {
+                            "first": "Primera",
+                            "last": "Ultima",
+                            "next": "Siguiente",
+                            "previous": "Anterior"
+                        },
+                        "aria": {
+                            "sortAscending": ": Activar ordenado por conlumna ascendente",
+                            "sortDescending": ": Activar ordenado por columna descendente"
+                        }
+                    }
+                });
+            }
+            ctrl.pagina = page;
+        }
 
         /** Estados.. */
         ctrl.std = function () {
@@ -24,7 +71,7 @@ angular.module("sacta_proxy")
         };
         ctrl.datestr = (date) => {
             var mdate = moment(date);
-            var str = mdate.format('DD-MM-YY HH:mm:ss');
+            var str = mdate.format('DD-MM-YY, HH:mm:ss');
             return {str};
         };
         ctrl.sect = (sect)=>{
@@ -43,6 +90,11 @@ angular.module("sacta_proxy")
         ctrl.logs = function () {
             var win = window.open('logs/logfile.csv', '_blank');
             win.focus();
+        };
+
+        ctrl.normalizeMap = (map) => {
+            var res = map.replace(/\s+/g, '');
+            return { str: res.replace(/,/g, ', ') };
         };
 
         $scope.$on('$viewContentLoaded', function () {
