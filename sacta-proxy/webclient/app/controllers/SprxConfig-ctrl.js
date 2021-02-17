05769f19b8e4f054b0bbca82cd32b05d50554d65
Arturo <Arturo@919647e8-3121-2f49-b100-017848d5475d>
2021-02-17 09:37:29 +0000

    Continuando con las paginas
    git-svn-id: http://10.34.2.66/svn/sacta-proxy/trunk@16 919647e8-3121-2f49-b100-017848d5475d

--- sacta-proxy/webclient/app/controllers/SprxConfig-ctrl.js
+++ sacta-proxy/webclient/app/controllers/SprxConfig-ctrl.js
@@ -6,42 +6,57 @@ angular.module("sacta_proxy")
         ctrl.pagina = 0;
         ctrl.config = undefined;
         ctrl.selecteddep = "0";
+        ctrl.selecteddep_mirror = "0";
         ctrl.dep = {
             cfg: null,
-            showselect: false,
-            showid: false,
-            showsectdata: false
+            isdep: false,
         };
         ctrl.topage = (page) => {
-            if (page == 1) {
-                if (ctrl.config) {
-                    ctrl.dep.cfg = ctrl.config.Psi;
-                    ctrl.dep.showselect= false;
-                    ctrl.dep.showid = false;
-                    ctrl.dep.showsectdata = false;
-                }
-            }
-            else if (page == 2) {
-                if (ctrl.config) {
-                    ctrl.dep.cfg = ctrl.config.Dependencies[ctrl.selecteddep];
-                    ctrl.dep.showselect = true;
-                    ctrl.dep.showid = true;
-                    ctrl.dep.showsectdata = true;
-                }
-            }
+            ConsolidateFromPage(ctrl.pagina);
+            DataLoadOfPage(page);
             ctrl.pagina = page;
         }
         ctrl.update = () => {
             alertify.success("Updating config..")
-
+            ConsolidateFromPage(ctrl.pagina);
         };
         ctrl.reset = () => {
+            /** Obtiene de nuevo la configuracion del servidor */
             load_config(() => {
                 /** reinicia los estados del form de configuracion */
                 $scope.cfgform.$setPristine();
-                ctrl.topage(ctrl.pagina);
+                DataLoadOfPage(ctrl.pagina);
             });
         };
+        ctrl.changedep = () => {
+            ConsolidateFromPage(ctrl.pagina);
+            ctrl.selecteddep_mirror = ctrl.selecteddep;
+            DataLoadOfPage(ctrl.pagina);
+        };
+
+        function ConsolidateFromPage(page) {
+            if (page == 1) {
+                ctrl.config.Psi = ctrl.dep.cfg;
+            }
+            else if (page == 2) {
+                ctrl.config.Dependencies[ctrl.selecteddep_mirror] = ctrl.dep.cfg;
+            }
+        }
+
+        function DataLoadOfPage(page) {
+            if (page == 1) {
+                if (ctrl.config) {
+                    ctrl.dep.cfg = ctrl.config.Psi;
+                    ctrl.dep.isdep = false;
+                }
+            }
+            else if (page == 2) {
+                if (ctrl.config) {
+                    ctrl.dep.cfg = ctrl.config.Dependencies[ctrl.selecteddep_mirror];
+                    ctrl.dep.isdep = true;
+                }
+            }
+        }
 
         function load_config(sync) {
             $serv.config((config) => {
