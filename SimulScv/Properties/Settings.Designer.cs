d0204b5a4763d9940868fca7fc6b815220da879e
Arturo <Arturo@919647e8-3121-2f49-b100-017848d5475d>
2021-03-10 16:00:43 +0000

    Introduce una latencia programable en los simuladores.
    git-svn-id: http://10.34.58.18/svn/sacta-proxy/trunk@103 919647e8-3121-2f49-b100-017848d5475d

--- SimulScv/Properties/Settings.Designer.cs
+++ SimulScv/Properties/Settings.Designer.cs
@@ -88,5 +88,14 @@ namespace SimulScv.Properties {
                 this["Posiciones"] = value;
             }
         }
+        
+        [global::System.Configuration.ApplicationScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute("50")]
+        public int LatenciaLan {
+            get {
+                return ((int)(this["LatenciaLan"]));
+            }
+        }
     }
 }
